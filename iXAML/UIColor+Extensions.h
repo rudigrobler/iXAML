73fea7102ea6c3070e81a9d67ce8ea87b620aeb1
rudigrobler <rudi@bbd.co.za>
2013-06-14 17:08:38 +0200

    Added todo tags with extra work

--- iXAML/UIColor+Extensions.h
+++ iXAML/UIColor+Extensions.h
@@ -2,4 +2,12 @@
 
 + (UIColor *)colorFromString:(NSString *)str;
 
+// TODO:
+// Currently expect #XXXXXX
+// Add alpha support
+// Short-hand ie. #fff = white?
+// Possible support for color names? ie. red, blue, etc?
+// Gradients?
+// Image as brush?
+
 @end
--- iXAML/UIFont+Extensions.h
+++ iXAML/UIFont+Extensions.h
@@ -4,4 +4,11 @@
 
 + (void)registerCustomFonts;
 
+// TODO:
+// +(UIFont)fontByString:(NSString*)fontNameAndSize
+//
+// "SegoeUI-Light 13px"
+// "SegoeUI-Bold 27"
+// "32 SegoeUI-SemiLight"
+
 @end
