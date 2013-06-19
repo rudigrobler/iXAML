6a0ba4abfb69c813b12f001d968d25846679227d
rudigrobler <rudi@bbd.co.za>
2013-06-19 15:43:45 +0200

    Update style parser

--- iXAML/UIView+Styling.m
+++ iXAML/UIView+Styling.m
@@ -1,3 +1,4 @@
+#import <QuartzCore/QuartzCore.h>
 #import "UIView+Styling.h"
 #import "UIColor+Extensions.h"
 #import "UIApplication+Styling.h"
@@ -19,43 +20,83 @@
     return objc_getAssociatedObject(self, @"___style");
 }
 
+- (void)applyStyle:(iXStyle*)style {
+    if (style) {        
+        for (NSString *property in style.keyEnumerator) {
+            NSString *value = [style valueForKey:property];
+            NSString *className = NSStringFromClass([self class]);
+
+            if ([className isEqualToString:@"UIView"])
+            {
+                CALayer *layer = [self layer];
+                if ([property isEqualToString:@"background-color"])
+                {
+                    [layer setBackgroundColor:[UIColor colorFromString:value].CGColor];
+                }
+            }
+            else if ([className isEqualToString:@"UIButton"])
+            {
+                CALayer *layer = [self layer];
+                if ([property isEqualToString:@"background-color"])
+                {
+                    [layer setBackgroundColor:[UIColor colorFromString:value].CGColor];
+                }
+                else if ([property isEqualToString:@"text-color"])
+                {
+                    UIButton *button = (UIButton*)self;
+                    [button.titleLabel setTextColor:[UIColor colorFromString:value]];
+                }
+                else if ([property isEqualToString:@"font"])
+                {
+                    UIButton *button = (UIButton*)self;
+                    [button.titleLabel setFont:[UIFont fontWithNameAndSize:value]];
+                }
+                else if ([property isEqualToString:@"border-color"])
+                {
+                    [layer setBorderColor:[UIColor colorFromString:value].CGColor];
+                }
+                else if ([property isEqualToString:@"border-width"])
+                {
+                    [layer setBorderWidth:[value floatValue]];
+                }
+                else if ([property isEqualToString:@"corner-radius"])
+                {
+                    [layer setCornerRadius:[value floatValue]];
+                }
+            }
+            else if ([className isEqualToString:@"UILabel"])
+            {
+                CALayer *layer = [self layer];
+                if ([property isEqualToString:@"background-color"])
+                {
+                    [layer setBackgroundColor:[UIColor colorFromString:value].CGColor];
+                }
+                else if ([property isEqualToString:@"text-color"])
+                {
+                    UILabel *label = (UILabel*)self;
+                    [label setTextColor:[UIColor colorFromString:value]];
+                }
+                else if ([property isEqualToString:@"font"])
+                {
+                    UILabel *label = (UILabel*)self;
+                    [label setFont:[UIFont fontWithNameAndSize:value]];
+                }
+            }
+            else
+            {
+                NSLog(@"%@", className);
+            }            
+        }
+    }
+}
+
 - (void)applyStyle {
     NSString *styleName = [self style];
     if (styleName) {
         iXStylesheet *stylesheet = [UIApplication sharedApplication].stylesheet;
         if (stylesheet) {
             iXStyle *style = [stylesheet valueForKey:styleName];
-            if (style) {
-                for (NSString *key in style.keyEnumerator) {
-                    id value = [style valueForKey:key];
-                    if ([key isEqualToString:@"backgroundColor"]) {
-                        UIColor *color = [UIColor colorFromString:value];
-                        if (color) {
-                            [self setValue:color forKey:key];
-                        }
-                    }
-                    else if ([key isEqualToString:@"textColor"]) {
-                        UIColor *color = [UIColor colorFromString:value];
-                        if (color) {
-                            if ([NSStringFromClass([self class]) isEqualToString:@"UIButton"]) {
-                                ((UIButton *) self).titleLabel.textColor = color;
-                            }
-                            else {
-                                [self setValue:color forKey:key];
-                            }
-                        }
-                    }
-                    else if ([key isEqualToString:@"font"]) {
-                        UIFont *font = [UIFont fontWithNameAndSize:value];
-                        if (font) {
-                            [self setValue:font forKey:key];
-                        }
-                    }
-                    else {
-                        NSLog(@"[%@:%@]", key, value);
-                    }
-                }
-            }
+            [self applyStyle:style];
         }
     }
 }
