0f95bb5278e1caea12e26936def2c9498832f68c
rudigrobler <rudi@bbd.co.za>
2013-06-20 21:53:43 +0200

    Added barButton & navBar support

---
+++ iXAML/UIBarButtonItem+Styling.h
@@ -0,0 +1,13 @@
+//
+//  UIBarButtonItem+Styling.h
+//  iXAML
+//
+//  Created by Rudi Grobler on 2013/06/20.
+//  Copyright (c) 2013 Rudi Grobler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIBarButtonItem (Styling)
+
+@end
---
+++ iXAML/UIBarButtonItem+Styling.m
@@ -0,0 +1,13 @@
+//
+//  UIBarButtonItem+Styling.m
+//  iXAML
+//
+//  Created by Rudi Grobler on 2013/06/20.
+//  Copyright (c) 2013 Rudi Grobler. All rights reserved.
+//
+
+#import "UIBarButtonItem+Styling.h"
+
+@implementation UIBarButtonItem (Styling)
+
+@end
