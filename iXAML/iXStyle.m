160863ae4541fac00c2eec064f028ed1c1b7f423
rudigrobler <rudi@bbd.co.za>
2013-06-22 00:32:49 +0200

    Add forwarding

--- iXAML/iXStyle.m
+++ iXAML/iXStyle.m
@@ -9,44 +9,82 @@
 
 - (id) init
 {
-    if (self = [super init])
-    {
-        self.proxy = [[NSMutableDictionary alloc] init];
-    }
+	if (self = [super init])
+	{
+		self.proxy = [[NSMutableDictionary alloc] init];
+	}
 
-    return self;
+	return self;
 }
-
 
 - (void) setObject:(id)obj forKey:(id)key
 {
-    if (obj)
-    {
-        [self.proxy setObject:obj forKey:key];
-    }
-    else
-    {
-        [self.proxy removeObjectForKey:key];
-    }
+	if (obj)
+	{
+		[self.proxy setObject:obj forKey:key];
+	}
+	else
+	{
+		[self.proxy removeObjectForKey:key];
+	}
 }
-
 
 - (id) objectForKey:(id)aKey
 {
-    return [self.proxy objectForKey:aKey];
+	return [self.proxy objectForKey:aKey];
 }
-
 
 - (NSUInteger) count
 {
-    return self.proxy.count;
+	return self.proxy.count;
 }
-
 
 - (NSEnumerator *) keyEnumerator
 {
-    return self.proxy.keyEnumerator;
+	return self.proxy.keyEnumerator;
 }
 
+#pragma mark - Forwarding
+
+- (BOOL)respondsToSelector:(SEL)aSelector
+{
+	if ( [super respondsToSelector:aSelector] )
+		return YES;
+
+	if ([self.proxy respondsToSelector:aSelector])
+		return YES;
+
+	return NO;
+}
+
+- (NSMethodSignature*) methodSignatureForSelector:(SEL)selector
+{
+	NSMethodSignature* signature = [super methodSignatureForSelector:selector];
+
+	if (!signature)
+		signature = [self.proxy methodSignatureForSelector:selector];
+
+	return signature;
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+	NSLog(@"forwardInvocation: %@ %@", NSStringFromClass([self class]), NSStringFromSelector([anInvocation selector]));
+
+	if ([self.proxy respondsToSelector:[anInvocation selector]])
+	{
+		[anInvocation invokeWithTarget:_proxy];
+	}
+	else
+	{
+		[super forwardInvocation:anInvocation];
+	}
+}
+
+- (id)forwardingTargetForSelector:(SEL)sel
+{
+	if ([self.proxy respondsToSelector:sel])
+		return self.proxy;
+	return nil;
+}
 
 @end
--- iXAML/iXStylesheet.m
+++ iXAML/iXStylesheet.m
@@ -8,109 +8,144 @@
 @end
 
 @implementation iXStylesheet {
-    NSMutableDictionary *_currentElement;
-    NSXMLParser *_parser;
+	NSMutableDictionary *_currentElement;
+	NSXMLParser *_parser;
 }
 
 - (id) init
 {
-    if (self = [super init])
-    {
-        self.proxy = [[NSMutableDictionary alloc] init];
-    }
+	if (self = [super init])
+	{
+		self.proxy = [[NSMutableDictionary alloc] init];
+	}
 
-    return self;
+	return self;
 }
-
 
 - (void) setObject:(id)obj forKey:(id)key
 {
-    if (obj)
-    {
-        [self.proxy setObject:obj forKey:key];
-    }
-    else
-    {
-        [self.proxy removeObjectForKey:key];
-    }
+	if (obj)
+	{
+		[self.proxy setObject:obj forKey:key];
+	}
+	else
+	{
+		[self.proxy removeObjectForKey:key];
+	}
 }
-
 
 - (id) objectForKey:(id)aKey
 {
-    return [self.proxy objectForKey:aKey];
+	return [self.proxy objectForKey:aKey];
 }
-
 
 - (NSUInteger) count
 {
-    return self.proxy.count;
+	return self.proxy.count;
 }
-
 
 - (NSEnumerator *) keyEnumerator
 {
-    return self.proxy.keyEnumerator;
+	return self.proxy.keyEnumerator;
 }
-
 
 #pragma mark - XAML
 
 - (void) parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict
 {
-    if ([elementName isEqualToString:@"Style"])
-    {
-        _currentElement = [[NSMutableDictionary alloc] init];
-        [_currentElement setValue:[attributeDict valueForKey:@"name"] forKey:@"style-name"];
-    }
-    else if ([elementName isEqualToString:@"Setter"])
-    {
-        NSString *property = [attributeDict valueForKey:@"property"];
-        NSString *state = [attributeDict valueForKey:@"state"];
-        if (state)
-        {
-            property = [NSString stringWithFormat:@"%@[%@]", property, state];
-        }
+	if ([elementName isEqualToString:@"Style"])
+	{
+		_currentElement = [[NSMutableDictionary alloc] init];
+		[_currentElement setValue:[attributeDict valueForKey:@"name"] forKey:@"style-name"];
+	}
+	else if ([elementName isEqualToString:@"Setter"])
+	{
+		NSString *property = [attributeDict valueForKey:@"property"];
+		NSString *state = [attributeDict valueForKey:@"state"];
+		if (state)
+		{
+			property = [NSString stringWithFormat:@"%@[%@]", property, state];
+		}
 
-        [_currentElement setValue:[attributeDict valueForKey:@"value"] forKey:property];
-    }
+		[_currentElement setValue:[attributeDict valueForKey:@"value"] forKey:property];
+	}
 }
-
 
 - (void) parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName
 {
-    if ([elementName isEqualToString:@"Style"])
-    {
-        NSString *styleName = [_currentElement valueForKey:@"style-name"];
-        iXStyle *style = [[iXStyle alloc] init];
+	if ([elementName isEqualToString:@"Style"])
+	{
+		NSString *styleName = [_currentElement valueForKey:@"style-name"];
+		iXStyle *style = [[iXStyle alloc] init];
 
-        for (NSString *key in [_currentElement keyEnumerator])
-        {
-            [style setValue:[_currentElement valueForKey:key] forKey:key];
-        }
+		for (NSString *key in [_currentElement keyEnumerator])
+		{
+			[style setValue:[_currentElement valueForKey:key] forKey:key];
+		}
 
-        [_proxy setValue:style forKey:styleName];
-        _currentElement = nil;
-    }
-    else if ([elementName isEqualToString:@"Stylesheet"])
-    {
-    }
+		[_proxy setValue:style forKey:styleName];
+		_currentElement = nil;
+	}
+	else if ([elementName isEqualToString:@"Stylesheet"])
+	{
+	}
 }
-
 
 - (id) initWithXAML:(NSURL *)url
 {
-    if (self = [super init])
-    {
-        _proxy = [[NSMutableDictionary alloc] init];
-        _parser = [[NSXMLParser alloc] initWithContentsOfURL:url];
+	if (self = [super init])
+	{
+		_proxy = [[NSMutableDictionary alloc] init];
+		_parser = [[NSXMLParser alloc] initWithContentsOfURL:url];
 
-        [_parser setDelegate:self];
-        [_parser parse];
-    }
+		[_parser setDelegate:self];
+		[_parser parse];
+	}
 
-    return self;
+	return self;
 }
 
+#pragma mark - Forwarding
+
+- (BOOL)respondsToSelector:(SEL)aSelector
+{
+	if ( [super respondsToSelector:aSelector] )
+		return YES;
+
+	if ([self.proxy respondsToSelector:aSelector])
+		return YES;
+
+	return NO;
+}
+
+- (NSMethodSignature*) methodSignatureForSelector:(SEL)selector
+{
+	NSMethodSignature* signature = [super methodSignatureForSelector:selector];
+
+	if (!signature)
+		signature = [self.proxy methodSignatureForSelector:selector];
+
+	return signature;
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+	NSLog(@"forwardInvocation: %@ %@", NSStringFromClass([self class]), NSStringFromSelector([anInvocation selector]));
+
+	if ([self.proxy respondsToSelector:[anInvocation selector]])
+	{
+		[anInvocation invokeWithTarget:_proxy];
+	}
+	else
+	{
+		[super forwardInvocation:anInvocation];
+	}
+}
+
+- (id)forwardingTargetForSelector:(SEL)sel
+{
+	if ([self.proxy respondsToSelector:sel])
+		return self.proxy;
+	return nil;
+}
 
 @end
