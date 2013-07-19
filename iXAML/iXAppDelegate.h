40186b3af4e05ff3075572ccc471c83e23aa07bf
rudigrobler <rudi@bbd.co.za>
2013-07-19 02:48:00 +0200

    Simplified project structure for sharing

--- iXAML/iXAppDelegate.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface iXAppDelegate : UIResponder <UIApplicationDelegate>
-
-@property(strong, nonatomic) UIWindow *window;
-
-@end
--- iXAML/iXComplexViewController.h
+++
@@ -1,5 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface iXComplexViewController : UIViewController
-
-@end
